c811a281f3ffa473b8befc771df070bdac2695b4
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2017-11-09 00:06:36 +0600

    add simple array sum solution

---
+++ hackerrank/Algorithms/Warmup/SimpleArraySum.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+/**
+ * Created by vsushko on 09.11.2017
+ */
+public class SimpleArraySum {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int arr[] = new int[n];
+
+        for (int arr_i = 0; arr_i < n; arr_i++) {
+            arr[arr_i] = in.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int arr_i = 0; arr_i < n; arr_i++) {
+            sum += arr[arr_i];
+        }
+
+        System.out.println(sum);
+    }
+}
