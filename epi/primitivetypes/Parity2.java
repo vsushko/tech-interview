0acdd9923e2d7cfdeb1609cd5fc2466bd12696f1
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-01-04 17:50:00 +0600

    add solutions for primitive types chapter

---
+++ epi/primitivetypes/Parity2.java
@@ -0,0 +1,14 @@
+package primitivetypes;
+
+public class Parity2 {
+    // @include
+    public static short parity(long x) {
+        short result = 0;
+        while (x != 0) {
+            result ^= 1;
+            x &= (x - 1); // Drops the lowest set bit of x.
+        }
+        return result;
+    }
+    // @exclude
+}
