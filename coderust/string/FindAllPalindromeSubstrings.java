9350228c47da2d395f0f37777d525ebf64507311
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-02-12 20:44:25 +0600

    add find all palindrome substrings solution 2

--- coderust/string/FindAllPalindromeSubstrings.java
+++ coderust/string/FindAllPalindromeSubstrings.java
@@ -4,7 +4,8 @@ public class FindAllPalindromeSubstrings {
 
     public static void main(String[] args) {
         String test = "aabbbaa";
-        findAllPalindromeSubstrings(test);
+        System.out.println(findAllPalindromeSubstrings(test));
+        System.out.println(findAllPalindromeSubstrings2(test));
     }
 
     private static boolean isPalindrome(String input, int i, int j) {
@@ -23,11 +24,35 @@ public class FindAllPalindromeSubstrings {
         for (int i = 0; i < input.length(); i++) {
             for (int j = i + 1; j < input.length(); j++) {
                 if (isPalindrome(input, i, j)) {
-                    System.out.println(input.substring(i, j + 1));
+                    System.out.print(input.substring(i, j + 1) + ", ");
                     count++;
                 }
             }
         }
+        System.out.println();
+        return count;
+    }
+
+    private static int findPalindromesInSubString(String input, int j, int k) {
+        int count = 0;
+
+        for (; j >= 0 && k < input.length(); --j, ++k) {
+            if (input.charAt(j) != input.charAt(k)) {
+                break;
+            }
+            System.out.print(input.substring(j, k + 1) + ", ");
+            count++;
+        }
+        return count;
+    }
+
+    private static int findAllPalindromeSubstrings2(String input) {
+        int count = 0;
+        for (int i = 0; i < input.length(); i++) {
+            count += findPalindromesInSubString(input, i - 1, i + 1);
+            count += findPalindromesInSubString(input, i, i + 1);
+        }
+        System.out.println();
         return count;
     }
 }
