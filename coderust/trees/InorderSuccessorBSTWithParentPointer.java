d91ad3c04e384aec04a259307d3ee9857575854b
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-03-01 17:35:37 +0600

    added class for inorder successro bst with part pointer solution

---
+++ coderust/trees/InorderSuccessorBSTWithParentPointer.java
@@ -0,0 +1,9 @@
+package trees;
+
+/**
+ * Inorder successor of a node in binary tree is the next node in inorder traversal.
+ * Write a method to find inorder successor of a given binary tree node in
+ * binary search tree given parent pointers.
+ */
+public class InorderSuccessorBSTWithParentPointer {
+}
