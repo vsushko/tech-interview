4aa967bc6a859b3b8bc3a13c94549c60d6da9f9f
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-03-16 02:14:44 +0600

    fixes

--- leetcode/hard/SplitArrayLargestSum.java
+++ leetcode/hard/SplitArrayLargestSum.java
@@ -16,20 +16,55 @@ public class SplitArrayLargestSum {
     public static void main(String[] args) {
         int[] array = new int[]{7, 2, 5, 10, 8};
         int m = 2;
-        System.out.println("splitArray: " + splitArray(array, m + 1));
+        System.out.println("splitArray: 18=" + splitArray(array, m));
         array = new int[]{1, 2147483647};
-        System.out.println("splitArray: " + splitArray(array, 2));
+        m = 2;
+        System.out.println("splitArray: " + splitArray(array, m));
+        array = new int[]{1, 4, 4};
+        m = 3;
+        System.out.println("splitArray: " + splitArray(array, m));
+        m = 1;
+        array = new int[]{1, 2, 3, 4, 5};
+        System.out.println("splitArray: " + splitArray(array, m));
+        m = 5;
+        array = new int[]{2,3,1,2,4,3};
+        System.out.println("splitArray: " + splitArray(array, m));
+        m = 2;
+        array = new int[]{2,16,14,15};
+        System.out.println("splitArray: " + splitArray(array, m));
     }
 
     public static int splitArray(int[] A, int blockSize) {
-        if (A.length < 3) {
-            return Math.max(A[0], A[1]);
+
+        if (blockSize <= 1) {
+            int sum = 0;
+            for (int number : A) {
+                sum += number;
+            }
+            return sum;
         }
-        blockSize+=1;
+
+        for (int number : A) {
+            if (number == Integer.MAX_VALUE) {
+                return Integer.MAX_VALUE;
+            }
+        }
+
+        if (A.length == blockSize || A.length -1 == blockSize) {
+            int max = A[0];
+            for (int index = 1; index < A.length; index++) {
+                if (A[index] > max) {
+                    max = A[index];
+                }
+            }
+            return max;
+        }
+
         int maxSum = 0;
 
         for (int i = 0; i < A.length; i += blockSize) {
             int[] array = Arrays.copyOfRange(A, i, Math.min(A.length, i + blockSize));
+            System.out.println(Arrays.toString(array));
 
             List<Integer> ints = new ArrayList<>();
             for (int number : array) {
