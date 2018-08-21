423290aeeeb97460caa00851eb186ed2e975b656
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-08-21 20:02:29 +0300

    added valid parentheses solution

---
+++ leetcode/easy/ValidParentheses.java
@@ -0,0 +1,37 @@
+package easy;
+
+import java.util.Stack;
+
+/**
+ * 20. Valid Parentheses
+ *
+ * @author vsushko
+ */
+public class ValidParentheses {
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("([)]"));
+        System.out.println(isValid("{[]}"));
+    }
+
+    private static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != c) {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
