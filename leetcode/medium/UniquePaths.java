16748cc1e2fb0439b97be0bec11a35031b80546a
Vasiliy Sushko <vasiliy.sushko@gmail.com>
2018-09-18 00:51:33 +0300

    added unique paths solution

---
+++ leetcode/medium/UniquePaths.java
@@ -0,0 +1,34 @@
+package medium;
+
+/**
+ * 62. Unique Paths
+ *
+ * @author vsushko
+ */
+public class UniquePaths {
+
+    public static void main(String[] args) {
+        System.out.println("uniquePaths(3, 2): " + uniquePaths(3, 2));
+        System.out.println("uniquePaths(7, 3): " + uniquePaths(7, 3));
+    }
+
+    private static int uniquePaths(int m, int n) {
+        int[][] array = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            array[i][0] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            array[0][i] = 1;
+        }
+
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                array[i][j] = array[i - 1][j] + array[i][j - 1];
+            }
+        }
+
+        return array[m - 1][n - 1];
+    }
+}
